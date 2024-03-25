875d8e985c9cef378ce2293fc494d31c612016c3
lalit-git-hub98 <lalitwale2006@gmail.com>
2024-03-25 17:40:37 -0400

    Update README file

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from PIL import Image
 load_dotenv()
 
 logo = Image.open('icon.jfif')
-st.set_page_config(page_title = 'Simple ChatBot', page_icon = logo)
+st.set_page_config(page_title = 'ChatBot', page_icon = logo)
 
 #st.title('Simple ChatBot using LangChain')
-st.markdown("<h1 style='text-align:center; color:black;'>Simple ChatBot using LangChain</h1>", unsafe_allow_html = True)
+st.markdown("<h1 style='text-align:center; color:black;'>ChatBot using LangChain</h1>", unsafe_allow_html = True)
 
 with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -21,8 +21,8 @@ response_button = st.button('Get Response', type = 'primary')
 
 
 if response_button:
-    #llm = OpenAI(temperature = 0.6, openai_api_key = os.getenv('OPENAI_API_KEY'))
-    llm = OpenAI(temperature = 0.6, openai_api_key = 'OPEN')
+    llm = OpenAI(temperature = 0.6, openai_api_key = os.getenv('OPENAI_API_KEY'))
+    #llm = OpenAI(temperature = 0.6, openai_api_key = 'OPEN')
     response = llm(input)
     st.subheader('The respone is')
     st.write(response)
